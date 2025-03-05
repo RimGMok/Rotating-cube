c53f3f1869c0265d381cad7630affa93a4d126a2
Louis <louis.v.092006@gmail.com>
2025-03-05 16:56:09 +0100

    fixed

--- rotating_cube.c
+++ rotating_cube.c
@@ -5,42 +5,60 @@
 #define WINDOW_WIDTH 500
 #define WINDOW_HEIGHT 500
 
-void draw_lines(SDL_Renderer* renderer, float points[][2],int size_tab){
-	
-	SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
-	
-	for (int i = 0 ; i < size_tab ; i++ ) {
-		SDL_RenderDrawLine(renderer, 
-						points[i][0],
-						points[i][1],
-						points[(i+1)%size_tab][0],
-						points[(i+1)%size_tab][1]);
-	}
-}
+typedef struct {
+    float x, y, z;
+} Point3D;
 
-void new_coordinates(float points[][2], int size_tab, float angle, float centerX, float centerY) {
+void draw_lines(SDL_Renderer* renderer, float points[][2], int size_tab) {
+    SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
     for (int i = 0; i < size_tab; i++) {
-		
-		// CENTERED POINTS
-        float x = points[i][0] - centerX;
-        float y = points[i][1] - centerY;
-        
-		// ROTATION ON ITSELF
-        points[i][0] = x * cos(angle) - y * sin(angle) + centerX;
-        points[i][1] = x * sin(angle) + y * cos(angle) + centerY;
+        SDL_RenderDrawLine(renderer, 
+                           points[i][0], points[i][1],
+                           points[(i+1)%size_tab][0], points[(i+1)%size_tab][1]);
     }
 }
 
+void project(Point3D points3D[], float points2D[][2], int size_tab, float distance) {
+    for (int i = 0; i < size_tab; i++) {
+        points2D[i][0] = (points3D[i].x * distance) / (points3D[i].z + distance) + WINDOW_WIDTH / 2;
+        points2D[i][1] = (points3D[i].y * distance) / (points3D[i].z + distance) + WINDOW_HEIGHT / 2;
+    }
+}
+
+void rotateX(Point3D points[], int size_tab, float angle) {
+    for (int i = 0; i < size_tab; i++) {
+        float y = points[i].y;
+        float z = points[i].z;
+        points[i].y = y * cos(angle) - z * sin(angle);
+        points[i].z = y * sin(angle) + z * cos(angle);
+    }
+}
+
+void rotateY(Point3D points[], int size_tab, float angle) {
+    for (int i = 0; i < size_tab; i++) {
+        float x = points[i].x;
+        float z = points[i].z;
+        points[i].x = x * cos(angle) + z * sin(angle);
+        points[i].z = -x * sin(angle) + z * cos(angle);
+    }
+}
+
+void rotateZ(Point3D points[], int size_tab, float angle) {
+    for (int i = 0; i < size_tab; i++) {
+        float x = points[i].x;
+        float y = points[i].y;
+        points[i].x = x * cos(angle) - y * sin(angle);
+        points[i].y = x * sin(angle) + y * cos(angle);
+    }
+}
 
 int main(int argc, char *argv[]) {
-    // INIT SDL
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         printf("SDL Initialization Error: %s\n", SDL_GetError());
         return 1;
     }
 
-    // CREATING THE WINDOW
-    SDL_Window *window = SDL_CreateWindow("Rotating cube", 
+    SDL_Window *window = SDL_CreateWindow("Rotating 3D Cube", 
                                           SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
                                           WINDOW_WIDTH, WINDOW_HEIGHT, 
                                           SDL_WINDOW_SHOWN);
@@ -50,7 +68,6 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // RENDERER
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) {
         printf("Renderer Creation Error: %s\n", SDL_GetError());
@@ -59,54 +76,51 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // BLACK BACKGROUND
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
-	
-	// POINTS
-	
-	float points[][2] = {
-		{300,300},
-		{300,200},
-		{200,200},
-		{200,300}};
-	
-	float angle = 0.02f;
-	float centerX = (points[0][0] + points[2][0]) / 2;
-    float centerY = (points[0][1] + points[2][1]) / 2;
-	
-	SDL_Event e;
-		int quit = 0;
-		while (!quit) {
-			while (SDL_PollEvent(&e)) {
-				if (e.type == SDL_QUIT) {
-					quit = 1;
-				}
-			}
-			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); 
-			SDL_RenderClear(renderer);
-			draw_lines(renderer, points, 4);
-			new_coordinates(points, 4, angle, centerX, centerY);
-			SDL_RenderPresent(renderer);
-			SDL_Delay(16);
-			//angle += 0.001f;
 
-		}
+    Point3D points3D[8] = {
+        {100, 100, 100}, {100, -100, 100}, {-100, -100, 100}, {-100, 100, 100},
+        {100, 100, -100}, {100, -100, -100}, {-100, -100, -100}, {-100, 100, -100}
+    };
 
+    int edges[12][2] = {
+        {0, 1}, {1, 2}, {2, 3}, {3, 0},
+        {4, 5}, {5, 6}, {6, 7}, {7, 4},
+        {0, 4}, {1, 5}, {2, 6}, {3, 7}
+    };
 
+    float points2D[8][2];
+    float angleX = 0.02f, angleY = 0.02f, angleZ = 0.02f;
+    float distance = 500;
 
-    // EXIT EVENT
-    SDL_Event event;
-    int running = 1;
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                running = 0;
+    SDL_Event e;
+    int quit = 0;
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                quit = 1;
             }
         }
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        rotateX(points3D, 8, angleX);
+        rotateY(points3D, 8, angleY);
+        rotateZ(points3D, 8, angleZ);
+        project(points3D, points2D, 8, distance);
+
+        SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+        for (int i = 0; i < 12; i++) {
+            SDL_RenderDrawLine(renderer,
+                               points2D[edges[i][0]][0], points2D[edges[i][0]][1],
+                               points2D[edges[i][1]][0], points2D[edges[i][1]][1]);
+        }
+
+        SDL_RenderPresent(renderer);
+        SDL_Delay(16);
     }
 
-    // CLEANING
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
